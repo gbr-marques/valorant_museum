09bb37c45c431d97fea91f512619d11f20e62cfe
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2025-05-28 21:08:14 -0300

    chore: structure category object list

---
+++ src/data/categories.ts
@@ -0,0 +1,48 @@
+export type Category = {
+    name: string;
+    icon: string;
+    slug: string;
+  };
+  
+  export const categories: Category[] = [
+    {
+      name: 'Agents',
+      icon: '/icons/agents.png',
+      slug: 'agents',
+    },
+    {
+      name: 'Maps',
+      icon: '/icons/maps.png',
+      slug: 'maps',
+    },
+    {
+      name: 'Weapons',
+      icon: '/icons/weapons.png',
+      slug: 'weapons',
+    },
+    {
+      name: 'Gamemodes',
+      icon: '/icons/gamemodes.png',
+      slug: 'gamemodes',
+    },
+    {
+      name: 'Player cards',
+      icon: '/icons/playercards.png',
+      slug: 'playercards',
+    },
+    {
+      name: 'Tiers',
+      icon: '/icons/tiers.png',
+      slug: 'tiers',
+    },
+    {
+      name: 'Sprays',
+      icon: '/icons/sprays.png',
+      slug: 'sprays',
+    },
+    {
+      name: 'Bundles',
+      icon: '/icons/bundles.png',
+      slug: 'bundles',
+    },
+  ];
