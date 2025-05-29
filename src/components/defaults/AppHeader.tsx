143f624131cffbfb2c705ea4590f61270417d6ea
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2025-05-29 10:49:25 -0300

    chore: add application header

---
+++ src/components/defaults/AppHeader.tsx
@@ -0,0 +1,87 @@
+import { categories } from "@/data/categories";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Menubar } from "primereact/menubar";
+
+const AppHeader = () => {
+  const router = useRouter();
+
+  const menuItems = [
+    {
+      label: "Home",
+      icon: "pi pi-home",
+    },
+    {
+      label: "Features",
+      icon: "pi pi-star",
+    },
+    {
+      label: "Projects",
+      icon: "pi pi-search",
+      items: [
+        {
+          label: "Components",
+          icon: "pi pi-bolt",
+        },
+        {
+          label: "Blocks",
+          icon: "pi pi-server",
+        },
+        {
+          label: "UI Kit",
+          icon: "pi pi-pencil",
+        },
+        {
+          label: "Templates",
+          icon: "pi pi-palette",
+          items: [
+            {
+              label: "Apollo",
+              icon: "pi pi-palette",
+            },
+            {
+              label: "Ultima",
+              icon: "pi pi-palette",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Contact",
+      icon: "pi pi-envelope",
+    },
+  ];
+
+  //   const menuItems = [
+  //     {
+  //       label: "Categorias",
+  //       items: categories.map((category) => ({
+  //         label: category.name,
+  //         command: () => router.push(`/categories/${category.slug}`),
+  //       })),
+  //     },
+  //     {
+  //       label: "Quiz",
+  //       command: () => router.push("/quiz"),
+  //     },
+  //     {
+  //       label: "About",
+  //       command: () => router.push("/about"),
+  //     },
+  //   ];
+
+  return (
+    <nav className="w-full flex items-center justify-center absolute h-fit p-8">
+      <Image
+        className="max-h-[60px]"
+        width={300}
+        height={100}
+        src={"/img/app_logo.svg"}
+        alt="Valorant Museum logo"
+      ></Image>
+    </nav>
+  );
+};
+
+export default AppHeader;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,12 @@ import "primeicons/primeicons.css";
 import type { AppProps } from "next/app";
 import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
 import AppFooter from "@/components/defaults/AppFooter";
+import AppHeader from "@/components/defaults/AppHeader";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PrimeReactProvider>
+      <AppHeader></AppHeader>
       <Component {...pageProps} />
       <AppFooter></AppFooter>
     </PrimeReactProvider>
