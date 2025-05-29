377fd654f4832a3b7d7e67b7377330bc766c469e
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2025-05-29 11:43:16 -0300

    fix: adjust agents category icon resolution

--- src/components/sections/QuizSection.tsx
+++ src/components/sections/QuizSection.tsx
@@ -3,7 +3,7 @@ import { Button } from "primereact/button";
 
 const QuizzSection = () => {
   return (
-    <section className="p-8 flex flex-col items-center justify-center h-[70dvh] md:h-[80dvh] gap-5 bg-[var(--bright-red)]">
+    <section className="p-8 md:py-[3rem] flex flex-col items-center justify-center h-[70dvh] md:h-[70dvh] gap-5 bg-[var(--bright-red)]">
       <Image height={500} width={500} src={'/img/quiz.png'} alt="Quix imagez"></Image>
       <div className="flex flex-col gap-5 text-center ">
         <h1 className="text-2xl text-white!">Lorem ipsum dolor sit amet consectetur.</h1>
