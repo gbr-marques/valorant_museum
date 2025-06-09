bcc7e22c722ae33a50030422a0cb2452614c033a
Gabriel Marques Silva <gabriel.m3008@gmail.com>
2025-06-09 14:55:14 -0300

    chore: pass category type to item card

--- src/pages/categories/[slug].tsx
+++ src/pages/categories/[slug].tsx
@@ -29,10 +29,10 @@ const categoryPage = () => {
 
   return (
     <section className="min-h-[90dvh] bg-red-50 p-6 pt-22 bg-[url('/img/bg/plain_bg.png')] bg-cover bg-center flex flex-col justify-center items-center">
-      <div className="bg-white w-full p-4 flex flex-col gap-3 rounded-3xl items-center justify-center  ">
+      <div className="bg-white w-full p-4 flex flex-col gap-6 rounded-3xl items-center justify-center  ">
         <h1 className="text-2xl rubik">{selectedCategory?.name}</h1>
         <div className="w-full grid grid-cols-2 gap-4 justify-between">
-          {itemList.map(item => <ItemCard></ItemCard>)}
+          {itemList.map(item => <ItemCard item={item} type={slug}></ItemCard>)}
         </div>
       </div>
     </section>
