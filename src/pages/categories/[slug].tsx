706ef2c4bffe0e6774e3a0120bd08f72f5ea76aa
Gabriel Marques Silva <gabriel.m3008@gmail.com>
2025-06-09 13:55:02 -0300

    chore: render cards based on item list

--- src/pages/categories/[slug].tsx
+++ src/pages/categories/[slug].tsx
@@ -19,9 +19,8 @@ const categoryPage = () => {
         (res) => res.json()
       );
       setItemList(res.data);
-      console.log(itemList);
     };
-    //getItemList()
+    getItemList()
   }, []);
 
   const selectedCategory = categories.find(
@@ -32,8 +31,8 @@ const categoryPage = () => {
     <section className="min-h-[90dvh] bg-red-50 p-6 pt-22 bg-[url('/img/bg/plain_bg.png')] bg-cover bg-center flex flex-col justify-center items-center">
       <div className="bg-white w-full p-4 flex flex-col gap-3 rounded-3xl items-center justify-center  ">
         <h1 className="text-2xl rubik">{selectedCategory?.name}</h1>
-        <div className="w-full grid grid-cols-2 gap-x-4 justify-between">
-          <ItemCard></ItemCard>
+        <div className="w-full grid grid-cols-2 gap-4 justify-between">
+          {itemList.map(item => <ItemCard></ItemCard>)}
         </div>
       </div>
     </section>
