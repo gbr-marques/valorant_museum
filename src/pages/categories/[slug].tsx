69751e9681df670621906bd00dc54af4e382443d
Gabriel Marques Silva <gabriel.m3008@gmail.com>
2025-06-09 13:47:39 -0300

    style: increase category page container border radius

--- src/pages/categories/[slug].tsx
+++ src/pages/categories/[slug].tsx
@@ -1,3 +1,4 @@
+import ItemCard from "@/components/ItemCard";
 import { categories } from "@/data/categories";
 import { useRouter } from "next/router";
 import { Button } from "primereact/button";
@@ -29,10 +30,10 @@ const categoryPage = () => {
 
   return (
     <section className="min-h-[90dvh] bg-red-50 p-6 pt-22 bg-[url('/img/bg/plain_bg.png')] bg-cover bg-center flex flex-col justify-center items-center">
-      <div className="bg-white w-full p-4 flex flex-col gap-3 rounded-xl items-center justify-center  ">
-        <h1 className="text-2xl">{selectedCategory?.name}</h1>
+      <div className="bg-white w-full p-4 flex flex-col gap-3 rounded-3xl items-center justify-center  ">
+        <h1 className="text-2xl rubik">{selectedCategory?.name}</h1>
         <div className="w-full grid grid-cols-2 gap-x-4 justify-between">
-          
+          <ItemCard></ItemCard>
         </div>
       </div>
     </section>
