dfc46d68cddd6b56b0510dd590810ac7ee514fa7
Gabriel Marques Silva <gabriel.m3008@gmail.com>
2025-06-09 15:06:23 -0300

    style: add conditional styling to certain categories

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -5,10 +5,14 @@ const ItemCard = ({ item, type }) => {
   return (
     <Card className="bg-gray-200! [&>div>div]:p-0! rounded-2xl! w-[150px]">
       <div className="flex flex-col gap-3 items-center">
-        <div
-            className="bg-red-50"
-        >
-          <img src={item.displayIcon} className={`rounded-md bg-linear-45 from-[var(--dark-red)] to-[var(--bright-red)] h-[110px] object-contain p-2`} alt="a" />
+        <div className="bg-red-50">
+          <img
+            src={item.displayIcon}
+            className={`rounded-md bg-linear-45 from-[var(--dark-red)] to-[var(--bright-red)] h-[110px] object-contain ${
+              ["agents", "sprays", "bundles"].includes(type) ? "p-0" : "p-2"
+            } ${type === "bundles" ? "object-cover" : ""}`}
+            alt="a"
+          />
         </div>
         <h3>{item.displayName}</h3>
         <Button
