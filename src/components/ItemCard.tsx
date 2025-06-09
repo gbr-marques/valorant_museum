63585dac3cd89520427e5d84d20827f1c7af7568
Gabriel Marques Silva <gabriel.m3008@gmail.com>
2025-06-09 14:54:36 -0300

    style: adjust item card style

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,18 +1,18 @@
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 
-const ItemCard = () => {
+const ItemCard = ({ item, type }) => {
   return (
-    <Card className="bg-gray-200! [&>div>div]:p-0! rounded-xl! w-[150px]">
+    <Card className="bg-gray-200! [&>div>div]:p-0! rounded-2xl! w-[150px]">
       <div className="flex flex-col gap-3 items-center">
-        <img
-          src="https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png"
-          className="bg-red-50 rounded-sm"
-          alt=""
-        />
-        <h3>Name</h3>
+        <div
+            className="bg-red-50"
+        >
+          <img src={item.displayIcon} className={`rounded-md bg-linear-45 from-[var(--dark-red)] to-[var(--bright-red)] h-[110px] object-contain p-2`} alt="a" />
+        </div>
+        <h3>{item.displayName}</h3>
         <Button
-          className="w-full bg-[var(--bright-red)]! text-sm!"
+          className="w-full bg-[var(--light-steel)]! text-sm!"
           label="See more"
         ></Button>
       </div>
