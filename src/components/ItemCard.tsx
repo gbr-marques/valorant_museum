8ec9816445c9976fa07948e5bd1634cb5a5137b5
Gabriel Marques Silva <gabriel.m3008@gmail.com>
2025-06-10 10:27:03 -0300

    style: adjust item card image position

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -8,15 +8,13 @@ const ItemCard = ({ item, type }) => {
   return (
     <Card className="bg-gray-200! [&>div>div]:p-0! rounded-2xl! w-[150px]">
       <div className="flex flex-col gap-3 items-center">
-        <div className="bg-red-50">
           <img
-            src={item.displayIcon}
-            className={`rounded-md bg-linear-45 from-[var(--dark-red)] to-[var(--bright-red)] h-[110px] object-contain ${
-              ["agents", "sprays", "bundles"].includes(type) ? "p-0" : "p-2"
-            } ${type === "bundles" ? "object-cover" : ""}`}
+            src={item.splash || item.displayIcon}
+            className={`w-full rounded-md bg-linear-45 from-[var(--dark-red)] to-[var(--bright-red)] h-[110px] object-contain ${
+              ["agents", "sprays", "bundles", "maps"].includes(type) ? "p-0" : "p-2"
+            } ${["bundles", "maps"].includes(type) ? "object-cover" : ""}`}
             alt="a"
           />
-        </div>
         <h3 className="rubik text-center">{item.displayName}</h3>
         <Button
           className="w-full bg-[var(--light-steel)]! text-sm!"
