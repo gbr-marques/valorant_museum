1d72fe2e4c7118b422114bcf664ee6d4ee256cfd
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-02-22 16:22:06 -0300

    refactor: change the file displayed on the cards icon

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -5,11 +5,13 @@ import { Card } from "primereact/card";
 const ItemCard = ({ item, categoryType }) => {
   const router = useRouter();
 
+ console.log(item)
+
   return (
     <Card className="bg-[var(--off-white)]! [&>div>div]:p-0! rounded-2xl! w-[150px] lg:w-full">
       <div className="flex flex-col gap-3 items-center">
           <img
-            src={item.splash || item.displayIcon}
+            src={item.listViewIcon || item.displayIcon}
             className={`w-full rounded-md bg-linear-45 from-[var(--dark-red)] to-[var(--bright-red)] object-contain rat aspect-[1/1] ${
               ["agents", "sprays", "bundles", "maps"].includes(categoryType) ? "p-0" : "p-2"
             } ${["bundles", "maps"].includes(categoryType) ? "object-cover" : ""}`}
